a110af010a6cf7c7d91c7e674a44e2bc4f2a5e46
yjt <1316515719@qq.com>
2020-09-20 11:07:02 +0800

    feat: 组件拆分

---
+++ src/main/java/com/trader/support/CurrencyManager.java
@@ -0,0 +1,39 @@
+package com.trader.support;
+
+import com.trader.entity.Currency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author yjt
+ * @since 2020/9/20 上午10:50
+ */
+public class CurrencyManager {
+
+    /**
+     * 货币映射MAP
+     */
+    private Map<String,Currency> currencyMap = new HashMap<>(16);
+
+    /**
+     * 添加一个货币
+     *
+     * @param currency 货币对象
+     */
+    public void addCurrency (Currency currency) {
+        Objects.requireNonNull(currency);
+        currencyMap.put(currency.getId(),currency);
+    }
+
+    /**
+     * 根据货币ID获取货币对象
+     *
+     * @param currencyId 货币ID
+     * @return 货币对象
+     */
+    public Currency getCurrency (String currencyId) {
+        return this.currencyMap.get(currencyId);
+    }
+}
--- src/main/java/com/trader/support/OrderBookManager.java
+++ src/main/java/com/trader/support/OrderBookManager.java
@@ -1,8 +1,58 @@
 package com.trader.support;
 
+import com.trader.entity.Order;
+import com.trader.entity.OrderBook;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author yjt
  * @since 2020/9/19 下午4:30
  */
 public class OrderBookManager {
+
+    /**
+     * 货币管理者
+     */
+    private CurrencyManager currencyMgr;
+
+    /**
+     * 产品管理器
+     */
+    private ProductManager productMgr;
+
+    /**
+     * 账本映射MAP
+     */
+    private Map<String,OrderBook> bookMap = new HashMap<>(16);
+
+    /**
+     * hide default constructor
+     */
+    private OrderBookManager () {}
+
+    public OrderBookManager (CurrencyManager currencyMgr,ProductManager productMgr) {
+        this.currencyMgr = Objects.requireNonNull(currencyMgr);
+        this.productMgr = Objects.requireNonNull(productMgr);
+    }
+
+    /**
+     * 根据订单获取该订单所在的账本
+     *
+     * @param order 订单
+     * @return 账本
+     */
+    public OrderBook getBook (Order order) {
+        OrderBook book = bookMap.get(order.getSymbol());
+        if (book == null) {
+            OrderBook newBook = new OrderBook();
+            newBook.setProduct(productMgr.getProduct(order.getProductId()));
+            newBook.setCurrency(currencyMgr.getCurrency(order.getCurrencyId()));
+            book = newBook;
+            bookMap.put(order.getSymbol(), newBook);
+        }
+        return book;
+    }
 }

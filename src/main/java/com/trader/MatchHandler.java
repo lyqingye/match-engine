8cfeab74a97acf6241faf88c8ddc3d0aafd91088
yjt <1316515719@qq.com>
2020-09-20 10:07:41 +0800

    style: 规范注释

--- src/main/java/com/trader/MatchHandler.java
+++ src/main/java/com/trader/MatchHandler.java
@@ -14,59 +14,67 @@ import com.trader.matcher.TradeResult;
 public interface MatchHandler {
 
     /**
-     * 产品添加事件处理器
-     * @param product
+     * 商品添加事件处理器
+     *
+     * @param product 商品
      * @throws Exception
      */
     default void onAddProduct (Product product) throws Exception {}
 
     /**
-     * 产品事件处理器
-     * @param product
+     * 商品事件处理器
+     *
+     * @param product 商品
      * @throws Exception
      */
     default void onDelProduct (Product product) throws Exception {}
 
     /**
      * 委托账本添加事件
-     * @param book
+     *
+     * @param book 账本
      * @throws Exception
      */
     default void onAddOrderBook (OrderBook book) throws Exception {}
 
     /**
      * 委托账本删除事件
-     * @param book
+     *
+     * @param book 账本
      * @throws Exception
      */
     default void onDelOrderBook (OrderBook book) throws Exception {}
 
     /**
      * 添加订单事件
-     * @param newOrder
+     *
+     * @param newOrder 订单
      * @throws Exception
      */
     default void onAddOrder (Order newOrder) throws Exception {}
 
     /**
      * 更新订单事件
-     * @param order
+     *
+     * @param order 订单
      * @throws Exception
      */
     default void onUpdateOrder (Order order) throws Exception {}
 
     /**
      * 删除订单事件
-     * @param order
+     *
+     * @param order 订单
      * @throws Exception
      */
     default void onDelOrder (Order order) throws Exception {}
 
     /**
      * 撮合订单事件
-     * @param order
-     * @param opponentOrder
-     * @param tradeResult
+     *
+     * @param order 订单
+     * @param opponentOrder 对手订单
+     * @param tradeResult 撮合结果
      * @throws Exception
      */
     default void onExecuteOrder(Order order, Order opponentOrder, TradeResult tradeResult) throws Exception {}

cfdf7e258cd746a95738b892f2752d6e4de7bfb0
yjt <1316515719@qq.com>
2020-09-29 18:06:59 +0800

    feat: save

--- src/main/java/com/trader/builder/LimitOrderBuilder.java
+++ src/main/java/com/trader/builder/LimitOrderBuilder.java
@@ -3,26 +3,33 @@ package com.trader.builder;
 import com.trader.def.OrderSide;
 import com.trader.entity.Order;
 
+import java.util.Date;
+
 /**
  * @author yjt
  * @since 2020/9/18 下午6:05
  */
 public class LimitOrderBuilder {
-    private Order order;
 
-    public static LimitOrderBuilder buy() {
-        LimitOrderBuilder builder = new LimitOrderBuilder();
-        builder.order = new Order();
-        builder.order.setSide(OrderSide.BUY);
-        return builder;
-    }
-
-    public static LimitOrderBuilder sell() {
-        LimitOrderBuilder builder = new LimitOrderBuilder();
-        builder.order = new Order();
-        builder.order.setSide(OrderSide.SELL);
+    public static LimitOrderBuilder buy(String symbolId) {
+        BuyLimitOrderBuilder builder = new BuyLimitOrderBuilder();
+        builder.order.setSymbol(symbolId);
         return builder;
     }
 
 
+
+
+    public static class BuyLimitOrderBuilder extends LimitOrderBuilder {
+        private Order order;
+
+        public BuyLimitOrderBuilder () {
+            order = new Order();
+            order.setSide(OrderSide.BUY);
+            order.setCreateDateTime(new Date());
+            order.setVersion(0);
+        }
+
+//        public BuyLimitOrderBuilder
+    }
 }

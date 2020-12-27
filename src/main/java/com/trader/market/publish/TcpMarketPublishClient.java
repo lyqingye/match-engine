ca5f5599bfb37ab2b92fe4bdd9778710e2ff1d79
lyqingye <1316515719@qq.com>
2020-12-27 17:32:12 +0800

    feat: 修正消息推送

--- src/main/java/com/trader/market/publish/TcpMarketPublishClient.java
+++ src/main/java/com/trader/market/publish/TcpMarketPublishClient.java
@@ -7,6 +7,7 @@ import com.trader.utils.VertxUtils;
 import com.trader.utils.messages.FrameParser;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.net.NetSocket;
@@ -20,198 +21,206 @@ import java.util.function.Consumer;
  * @since 2020/10/10 下午6:51
  */
 public class TcpMarketPublishClient implements MarketPublishClient {
-    /**
-     * 域名
-     */
-    private String host;
+  /**
+   * 域名
+   */
+  private String host;
 
-    /**
-     * 端口
-     */
-    private int port;
+  /**
+   * 端口
+   */
+  private int port;
 
-    /**
-     * 消费者
-     */
-    @Setter
-    private Consumer<Message<?>> consumer;
+  /**
+   * 消费者
+   */
+  @Setter
+  private Consumer<Message<?>> consumer;
 
-    /**
-     * socket
-     */
-    private NetSocket client;
+  /**
+   * socket
+   */
+  private NetSocket client;
 
-    /**
-     * 是否正在运行
-     */
-    private volatile boolean isRunning;
+  /**
+   * 是否正在运行
+   */
+  private volatile boolean isRunning;
 
-    /**
-     * 解析器
-     */
-    private FrameParser parser;
+  /**
+   * 创建市场管理器配置客户端
+   *
+   * @param host host
+   * @param port port
+   * @return client
+   */
+  public static MarketConfigHttpClient createConfigClient(String host, int port) {
+    return new MarketConfigHttpClient(VertxUtils.vertx(), host, port);
+  }
 
-    /**
-     * 创建市场管理器配置客户端
-     *
-     * @param host
-     *         host
-     * @param port
-     *         port
-     *
-     * @return client
-     */
-    public static MarketConfigHttpClient createConfigClient(String host, int port) {
-        return new MarketConfigHttpClient(VertxUtils.vertx(), host, port);
+  public TcpMarketPublishClient(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  /**
+   * 链接到目标
+   *
+   * @param host     域名
+   * @param port     端口
+   * @param consumer 消息消费者
+   */
+  @Override
+  public void conn(String host, int port, Consumer<Message<?>> consumer, Handler<AsyncResult<NetSocket>> connectHandler) {
+    this.host = host;
+    this.port = port;
+    this.consumer = consumer;
+    Vertx vertx = VertxUtils.vertx();
+    ThreadPoolUtils.submit(() -> {
+
+      final NetClientOptions options = new NetClientOptions();
+
+      // 设置重试次数和重试间隔时间
+      options.setReconnectInterval(1000)
+             .setReconnectAttempts(Integer.MAX_VALUE)
+             // 连接超时时间
+             .setConnectTimeout(5000)
+             // 保持长连接
+             .setTcpKeepAlive(true);
+
+      // 开始创建 socket 链接
+      vertx.createNetClient(options)
+           .connect(port, host, ar -> {
+             if (ar.succeeded()) {
+               // 连接成功
+               client = ar.result();
+               client.setWriteQueueMaxSize(4096);
+
+               if (connectHandler != null) {
+                 connectHandler.handle(ar);
+               }
+
+               System.out.println("[MarketPublish]: success connection to the server");
+               final FrameParser parser = new FrameParser(connAr -> {
+                 if (connAr.succeeded()) {
+                   if (this.consumer != null) {
+                     this.consumer.accept(connAr.result());
+                   }
+                 }
+               });
+               client.handler(parser);
+               this.isRunning = true;
+
+               client.exceptionHandler(Throwable::printStackTrace);
+
+               // 如果目标关闭则进行重连
+               client.closeHandler(close -> {
+                 this.client = null;
+                 if (this.isRunning) {
+                   conn(host, port, consumer, connectHandler);
+                 }
+               });
+             }
+           });
+    });
+
+    Buffer emptyBuffer = Buffer.buffer(5);
+    emptyBuffer.appendInt(1);
+    emptyBuffer.appendByte((byte) 0);
+//    vertx.setPeriodic(5000, timerId -> {
+//      send(emptyBuffer);
+//    });
+  }
+
+  /**
+   * 推送消息
+   *
+   * @param textMsg 文本消息
+   */
+  @Override
+  public void send(String textMsg) {
+    if (this.client != null) {
+      // 换行符是用于解决拆包黏包, 这里省事, 如果发送的消息是文本, 那就就以换行符当作包的终止符
+      this.client.write(textMsg + "\n", StandardCharsets.UTF_8.name());
+      // 解决背压问题, 关键字 `back pressure`, 想了解的话就去了解下吧
+      // 这边是选择丢弃消息
+      if (this.client.writeQueueFull()) {
+        this.client.pause();
+        this.client.drainHandler(done -> this.client.resume());
+      }
     }
+  }
 
-    public TcpMarketPublishClient(String host, int port) {
-        this.host = host;
-        this.port = port;
-        this.parser = new FrameParser(ar -> {
-            if (ar.succeeded()) {
-                if (this.consumer != null) {
-                    this.consumer.accept(ar.result());
-                }
-            }
-        });
-    }
+  @Override
+  public boolean isOpen() {
+    return this.client != null;
+  }
 
-    /**
-     * 链接到目标
-     *
-     * @param host
-     *         域名
-     * @param port
-     *         端口
-     * @param consumer
-     *         消息消费者
-     */
-    @Override
-    public void conn(String host, int port, Consumer<Message<?>> consumer, Handler<AsyncResult<NetSocket>> connectHandler) {
-        this.host = host;
-        this.port = port;
-        this.consumer = consumer;
-
-        ThreadPoolUtils.submit(() -> {
-
-            final NetClientOptions options = new NetClientOptions();
-
-            // 设置重试次数和重试间隔时间
-            options.setReconnectInterval(1000)
-                   .setReconnectAttempts(Integer.MAX_VALUE)
-                   // 保持长连接
-                   .setTcpKeepAlive(true);
-            // 开始创建 socket 链接
-            VertxUtils.vertx().createNetClient(options)
-                 .connect(port, host, ar -> {
-                     if (ar.succeeded()) {
-                         // 连接成功
-                         client = ar.result();
-                         client.setWriteQueueMaxSize(4096);
-
-                         if (connectHandler != null) {
-                             connectHandler.handle(ar);
-                         }
-
-                         System.out.println("[MarketPublish]: success connection to the server");
-
-                         client.handler(parser);
-                         this.isRunning = true;
-
-                         // 如果目标关闭则进行重连
-                         client.closeHandler(close -> {
-                             this.client = null;
-                             if (this.isRunning) {
-                                 conn(host, port, consumer, connectHandler);
-                             }
-                         });
-                     }
-                 });
-        });
-    }
-
-    /**
-     * 推送消息
-     *
-     * @param textMsg
-     *         文本消息
-     */
-    @Override
-    public void send(String textMsg) {
-        if (this.client != null) {
-            // 换行符是用于解决拆包黏包, 这里省事, 如果发送的消息是文本, 那就就以换行符当作包的终止符
-            this.client.write(textMsg + "\n", StandardCharsets.UTF_8.name());
-            // 解决背压问题, 关键字 `back pressure`, 想了解的话就去了解下吧
-            // 这边是选择丢弃消息
-            if (this.client.writeQueueFull()) {
-                this.client.pause();
-                this.client.drainHandler(done -> this.client.resume());
-            }
+  /**
+   * 推送消息
+   *
+   * @param binMsg 二进制消息
+   */
+  @Override
+  public void send(byte[] binMsg) {
+    if (this.client != null) {
+      this.client.write(Buffer.buffer(binMsg), ar -> {
+        if (ar.succeeded()) {
+          if (this.client.writeQueueFull()) {
+            this.client.pause();
+            this.client.drainHandler(done -> this.client.resume());
+          }
+        }else {
+          ar.cause().printStackTrace();
         }
+      });
     }
+  }
 
-    @Override
-    public boolean isOpen() {
-        return this.client != null;
-    }
-
-    /**
-     * 推送消息
-     *
-     * @param binMsg
-     *         二进制消息
-     */
-    @Override
-    public void send(byte[] binMsg) {
-        if (this.client != null) {
-            this.client.write(Buffer.buffer(binMsg));
-            if (this.client.writeQueueFull()) {
-                this.client.pause();
-                this.client.drainHandler(done -> this.client.resume());
-            }
+  /**
+   * 推送消息
+   *
+   * @param bufferMsg buffer
+   */
+  @Override
+  public void send(Buffer bufferMsg) {
+    if (this.client != null) {
+      this.client.write(bufferMsg,ar -> {
+        if (ar.succeeded()) {
+          if (this.client.writeQueueFull()) {
+            this.client.pause();
+            this.client.drainHandler(done -> this.client.resume());
+          }
+        }else {
+          ar.cause().printStackTrace();
         }
+      });
     }
+  }
 
-    /**
-     * 推送消息
-     *
-     * @param bufferMsg buffer
-     */
-    @Override
-    public void send(Buffer bufferMsg) {
-        if (this.client != null) {
-            this.client.write(bufferMsg);
-            if (this.client.writeQueueFull()) {
-                this.client.pause();
-                this.client.drainHandler(done -> this.client.resume());
-            }
-        }
+  /**
+   * 关闭链接
+   */
+  @Override
+  public void close() {
+    if (this.client != null) {
+      this.client.close();
+      this.isRunning = false;
     }
+  }
 
-    /**
-     * 关闭链接
-     */
-    @Override
-    public void close() {
-        if (this.client != null) {
-            this.client.close();
-            this.isRunning = false;
-        }
-    }
+  @Override
+  public String host() {
+    return this.host;
+  }
 
-    @Override
-    public String host() {
-        return this.host;
-    }
+  @Override
+  public int port() {
+    return this.port;
+  }
 
-    @Override
-    public int port() {
-        return this.port;
-    }
-
-    @Override
-    public Consumer<Message<?>> consumer() {
-        return this.consumer;
-    }
+  @Override
+  public Consumer<Message<?>> consumer() {
+    return this.consumer;
+  }
 }
